799fd574065ddf488e76befac68f733feea26993
zmbkilla <zmbkilla@DESKTOP-ON7EI42>
2024-01-16 14:54:23 +0800

    Fixed bugs with resoultion menu Changed the way the settings form is shown

--- SLauncher/Form1.Designer.cs
+++ SLauncher/Form1.Designer.cs
@@ -34,6 +34,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -49,13 +51,12 @@
             // 
             // gameD
             // 
-            this.gameD.AutoSize = true;
             this.gameD.BackColor = System.Drawing.SystemColors.ControlDark;
             this.gameD.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.gameD.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.gameD.Location = new System.Drawing.Point(74, 98);
             this.gameD.Name = "gameD";
-            this.gameD.Size = new System.Drawing.Size(2, 22);
+            this.gameD.Size = new System.Drawing.Size(620, 22);
             this.gameD.TabIndex = 1;
             // 
             // button1
@@ -75,7 +76,7 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
             this.button2.Image = global::SLauncher.Properties.Resources.test;
-            this.button2.Location = new System.Drawing.Point(100, 265);
+            this.button2.Location = new System.Drawing.Point(142, 274);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(144, 45);
             this.button2.TabIndex = 3;
@@ -86,7 +87,7 @@
             // button3
             // 
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.button3.Location = new System.Drawing.Point(333, 265);
+            this.button3.Location = new System.Drawing.Point(537, 274);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(144, 45);
             this.button3.TabIndex = 4;
@@ -94,21 +95,47 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Red;
+            this.button4.Location = new System.Drawing.Point(757, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(30, 30);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "X";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Enabled = false;
+            this.label2.Location = new System.Drawing.Point(46, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(681, 187);
+            this.label2.TabIndex = 6;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.BackgroundImage = global::SLauncher.Properties.Resources.BG;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.gameD);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Starlight Launcher";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -121,6 +148,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
     }
 }
 
