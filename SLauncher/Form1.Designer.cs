90d1a8530c20a7ce3195c9584184f76634b06639
zmbkilla <zmbkilla@DESKTOP-ON7EI42>
2024-01-17 01:13:50 +0800

    Changes to Launch and Download

--- SLauncher/Form1.Designer.cs
+++ SLauncher/Form1.Designer.cs
@@ -39,6 +39,7 @@
             this.button5 = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.Console1 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -147,6 +148,17 @@
             this.Console1.Size = new System.Drawing.Size(623, 42);
             this.Console1.TabIndex = 9;
             // 
+            // button6
+            // 
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button6.Location = new System.Drawing.Point(341, 220);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(144, 45);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Check hash";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -154,6 +166,7 @@
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.BackgroundImage = global::SLauncher.Properties.Resources.BG;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.Console1);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button5);
@@ -187,6 +200,7 @@
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Label Console1;
+        private System.Windows.Forms.Button button6;
     }
 }
 
